e337100921fc900fd47966d3464d064e971faa72
Dpingar <58369198+Dpingar@users.noreply.github.com>
2019-11-30 18:07:46 +0100

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "UpoBeer",
+    'summary': """Administración de UpoBeer""",
+    'description': """Administración de pedidos,clientes,cervezas...""",
+    'author': "Grupo 13",
+    'category': 'UpoBeer',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': ['views/cliente_view.xml','views/cerveza_view.xml','views/pedido_view.xml'],
+    'demo': [ ],
+    'application': True,
+}
